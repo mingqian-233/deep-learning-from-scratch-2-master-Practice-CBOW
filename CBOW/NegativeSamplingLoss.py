911a8ecb2c46d82a0e8fc216682a16ae28fd7e95
SkyStillBeBlue <1614195673@qq.com>
2025-01-16 01:11:41 +0800

    ...

--- CBOW/NegativeSamplingLoss.py
+++ CBOW/NegativeSamplingLoss.py
@@ -1,19 +1,49 @@
 import numpy as np
 
 
-class embedding:  # embed层其实就是取出其中一层（几层）的权重矩阵
+class Embedding:  # embed层其实就是取出权重矩阵中的其中一个词（几个词）
     def __init__(self, W):
         self.params = [W]
         self.grads = [np.zeros_like(W)]
         self.index = None
 
     def forward(self, index):
-        (W,) = self.params  # 注意这里有解包操作，要把Params拆封为一行行的
+        (W,) = self.params
+        # 注意这里有解包操作，如果不解包的话，W就是一个列表，而不是一个numpy矩阵。并且是浅拷贝。
         self.index = index
-        return W[index]
+        return W[index]  # index是列表，抽出这个列表里面的数字对应的行
 
     def backward(self, dout):
-        
+        dW = self.grads  # 这个地方直接引用（浅拷贝）了self.grads
+        dW[...] = 0
+        np.add.at(dW, self.index, dout)
+        return None
+
+
+class EmbeddingDot:
+    # 在上面抽取的基础上，加上了和h的点积
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        # 要先把权重矩阵转化为Embedding对象
+        se = self.embed
+        self.params = se.params
+        self.grads = se.grads
+        self.cache = None
+        # 用于存入正向传播的中间结果，以便反向传播时使用
+
+    def forward(self, h, index):
+        se = self.embed
+        targetW = se.forward(index)
+        out = np.sum(targetW * h, axis=1)
+        # 哈夫曼乘，结果是一个矩阵（因为用了mini_batch），然后再对这个矩阵的每一行求和，得到一个98行的向量，长度和mini_batch的大小一样。这个向量就是这一层的输出
+        self.cache = (h, targetW)
+        # 常识：圆括号括起来是元组，方括号括起来是列表
+        return out
+
+    def backward(self, dout):
+        h, targetW = self.cache
+        # dout是长为mini_batch的行向量，转置dout为列向量
+        dout = dout.reshape(dout.shape[0], 1)
 
 
 class UnigramSampler:
--- readme.md
+++ readme.md
@@ -2,3 +2,19 @@
 
 ## 描述
 个人在阅读书籍的时候的自行学习。common和dataset来源于原书 [deep-learning-from-scratch-2](https://github.com/oreilly-japan/deep-learning-from-scratch-2/tree/master)，其余代码大多数都为本人学习书本内容后自行编写。
+
+因为是完全看着书写的，所以写法肯定和书上别无二致。
+
+
+---
+
+## 对部分原代码的修改
+由于高版本的cupy已经不支持cupy.scatter_add（该功能自 CuPy v4 起已被弃用），需要从cupyx中调用，因此:
+
+
+1. 在np.py（原书用于控制是否开启GPU加速）中添加：
+```python
+    import cupyx as cpx
+```
+
+2. 修改common\layers.py下的Embedding层实现中的np.scatter_add(dW, self.idx, dout)为cpx.scatter_add(dW, self.idx, dout)
